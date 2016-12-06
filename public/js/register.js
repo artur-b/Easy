4b856277944fe425ded8e8881f77fb359386cbb5
Artur Brodowski <artur.brodowski@gmail.com>
2016-12-06 13:08:36 +0100

    fix styles, forms validation

--- public/js/register.js
+++ public/js/register.js
@@ -2,22 +2,13 @@ $(function() {
     $(".form-signup").validate({
         rules:
         {
-            name:"required",
-            email:"required email",
-            password:"required",
-            pesel:"required",
-            phone:"required",
             accept:"required"
         },
         messages:
         {
-            name:"Pole wymagane",
-            email:"Pole wymagane",
-            password:"Pole wymagane",
-            pesel:"Pole wymagane",
-            phone:"Pole wymagane",
-            accept:"Wymagana akceptacja regulaminu"
+            accept:"(wymagana akceptacja)"
         },
+        ignore: ".ignore",
         errorPlacement: function(error, element)
         {
             if (element.is(":checkbox")) {
@@ -29,8 +20,8 @@ $(function() {
     });
     $("#register-with-fb").click(function() {
         var v = $(".form-signup").validate();
-        console.log(v);
-        return false;        
+        var el = v.element("#accept");
+        return el;        
     });
 });
 
